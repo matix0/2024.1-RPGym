378c0baaf2c9d291619031f7542fc58ee8aebcaa
matix0 <mateusviniciuspw@gmail.com>
2024-09-09 18:26:27 -0300

    router validado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { Route, Routes } from "react-router-dom";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./styles/global.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Register from "./pages/Register";
 import Character from "./pages/Character";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Register" element={<Register />} />
-      <Route path="/Dashboard" element={<Dashboard />} />
-      <Route path="/Character" element={<Character />} />
+      <Route path="/" element={<ProtectedRoute element={<Login />} />} />
+      <Route
+        path="/register"
+        element={<ProtectedRoute element={<Register />} />}
+      />
+      <Route
+        path="/dashboard"
+        element={<ProtectedRoute element={<Dashboard />} />}
+      />
+      <Route
+        path="/character"
+        element={<ProtectedRoute element={<Character />} />}
+      />
     </Routes>
   );
 }
