2aeeff4c914a80e6ebb90192b356e15cf441a9b7
DaviRogs <davirocha12.80@gmail.com>
2024-09-09 16:32:49 -0300

    Ajustes no input

--- src/components/inputText/index.js
+++ src/components/inputText/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Input } from "./styles";
 
 function inputText(props) {
-  return <Input type={props.type} placeholder={props.placeholder} />;
+  return <Input type={props.type} placeholder={props.placeholder} onChange={props.onChange} value={props.value}/>;
 }
 
 export default inputText;
