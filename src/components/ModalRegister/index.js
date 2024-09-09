efdbe5e5bac3caaa956994c33c2b8d4d43e062cf
DaviRogs <davirocha12.80@gmail.com>
2024-09-09 16:14:31 -0300

    Ajustes no cadastro

--- src/components/ModalRegister/index.js
+++ src/components/ModalRegister/index.js
@@ -13,12 +13,29 @@ import InputText from "../inputText";
 import InputIdade from "../inputIdade/index.js";
 
 function ModalRegister() {
-  const [apelido, setApelido] = useState();
 
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [apelido, setApelido] = useState("");
+  const [idade, setIdade] = useState("");
+
+  // Função para capturar o apelido com apenas a primeira palavra
   const handleApelidoChange = (e) => {
-    const UniqueWord = e.target.value.split(" ")[0];
-    setApelido(UniqueWord);
+    const uniqueWord = e.target.value.split(" ")[0];
+    setApelido(uniqueWord);
   };
+
+  // Função de cadastro (simples validação)
+  const handleCadastro = () => {
+    if (senha !== confirmarSenha) {
+      alert("As senhas não coincidem!");
+
+      //Trabalha a integração dentro daqui
+
+      return;
+    }
+  }
 
   return (
     <Container>
@@ -27,21 +44,19 @@ function ModalRegister() {
           <Title>RPGym</Title>
           <Subtitle>Faça o cadastro e começe seu legado!</Subtitle>
         </DivDescricao>
-        <InputText type="email" placeholder="Email" />
-        <InputText placeholder="Senha" type="password" />
-        <InputText placeholder="Confirme a sua senha" type="password" />
+        <InputText type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+        <InputText placeholder="Senha" type="password" value={senha} onChange={(e) => setSenha(e.target.value)}/>
+        <InputText placeholder="Confirme a sua senha" type="password" value={confirmarSenha} onChange={(e) => setConfirmarSenha(e.target.value)}/>
         <DivFormsBaixo>
           <InputText
             placeholder="Apelido"
             value={apelido}
             onChange={handleApelidoChange}
           />
-          <InputIdade placeholder="Idade" />
+          <InputIdade placeholder="Idade" value={idade} onChange={(e) => setIdade(e.target.value)}/>
         </DivFormsBaixo>
         <Cadastrese
-          onClick={() => {
-            window.open("/character", "_self");
-          }}
+          onClick={handleCadastro}
         >
           Cadastre-se
         </Cadastrese>
